ca24a5770b4076ab6c39a2c3e3a0128c402fff24
Sumaiya Khondoker <sumaiyakhondoker85@gmail.com>
2026-09-02 21:09:18 +0600

    inserted table data and query 1,2,3,4 completed

--- QUERY.sql
+++ QUERY.sql
@@ -25,6 +25,41 @@ create table Bookings(
   )
 
 
+insert into users(full_name, role,phone_number) values
+('Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+('Asif Haque', 'asif@mail.com', 'Football Fan', '+88017222222'),
+('Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+88017333333'),
+('Jannat Ara', 'jannat@mail.com', 'Football Fan', null);
+
+insert into Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
+
+
+insert into Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150),
+(502, 1, 102, 'B-04', 'Confirmed', 120),
+(503, 2, 101, 'A-13', 'Confirmed', 150),
+(504, 2, 101, 'D-24', NULL, 150),
+(505, 3, 102, 'C-20', 'Pending', 120);
+
+
+-- query 1
+select match_id, fixture, base_ticket_price from Matches where tournament_category = 'Champions League' and match_status = 'Available';
+
+-- query 2
+select user_id, full_name, email from Users where full_name Ilike 'tanvir%' or full_name Ilike '%Haque%'  
+
+-- query-3
+select booking_id, user_id, match_id, coalesce(payment_status,'Action Required') as "systematic_status" from bookings where payment_status is null;
+
+-- query 4
+select booking_id, full_name, fixture, total_cost from Users inner join Bookings using(user_id) inner join matches using(match_id)
+
+
 
 
 
